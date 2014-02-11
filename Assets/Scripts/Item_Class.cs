c13cc8fca4250706788422f04c8e3e89c941452f
Jimmy562 <Billyjock321@btinternet.com>
2014-02-11 12:53:16 +0000

    Inventory

--- Assets/Scripts/Item_Class.cs
+++ Assets/Scripts/Item_Class.cs
@@ -22,26 +22,26 @@ public class Item_Class : MonoBehaviour {
 
 	// Trap Textures
 	public static Texture2D boxTrapIcon = (Texture2D)Resources.Load ("Textures/Traps/Box_Trap");
-	public static Texture2D bowIcon;
+	public static Texture2D bowIcon = (Texture2D)Resources.Load ("Textures/Traps/Bow_Trap");
 
 	// Items
-	public ItemClass nullItem = new ItemClass(0, 0, "null", nullIcon, "null");
-	public ItemClass stick_r = new ItemClass(1, 0, "Stick", stickIcon, "Can be used to make traps");
-	public ItemClass string_r = new ItemClass(2, 0, "String", stringIcon, "Can be used to make traps");
-	public ItemClass box_r = new ItemClass(3, 0, "Box", boxIcon, "Can be used to make traps");
-	public ItemClass rope_r = new ItemClass(4, 0, "Rope", ropeIcon, "Can be used to make traps");
-	public ItemClass rock_r = new ItemClass(5, 0, "Rock", rockIcon, "Can be used to make traps");
-	public ItemClass knife_r = new ItemClass(6, 0, "Knife", knifeIcon, "Can be used to make traps");
-	public ItemClass needle_r = new ItemClass(7, 0, "Needle", needleIcon, "Can be used to make traps");
-	public ItemClass balloon_r = new ItemClass(8, 0, "Balloon", balloonIcon, "Can be used to make traps");
-	public ItemClass cbattery_r = new ItemClass(9, 0, "Car Battery", cbatteryIcon, "Can be used to make traps");
-	public ItemClass wheel_r = new ItemClass(10, 0, "Wheel", wheelIcon, "Can be used to make traps");
-	public ItemClass hanger_r = new ItemClass(11, 0, "Clothes Hanger", hangerIcon, "Can be used to make traps");
-	public ItemClass alcohol_r = new ItemClass(12, 0, "Alcohol", alcoholIcon, "Can be used to make traps");
+	public ItemClass nullItem = new ItemClass(0, 0, "null", nullIcon, "null",false);
+	public ItemClass stick_r = new ItemClass(1, 0, "Stick", stickIcon, "Can be used to make traps",false);
+	public ItemClass string_r = new ItemClass(2, 0, "String", stringIcon, "Can be used to make traps",false);
+	public ItemClass box_r = new ItemClass(3, 0, "Box", boxIcon, "Can be used to make traps",false);
+	public ItemClass rope_r = new ItemClass(4, 0, "Rope", ropeIcon, "Can be used to make traps",false);
+	public ItemClass rock_r = new ItemClass(5, 0, "Rock", rockIcon, "Can be used to make traps",false);
+	public ItemClass knife_r = new ItemClass(6, 0, "Knife", knifeIcon, "Can be used to make traps",false);
+	public ItemClass needle_r = new ItemClass(7, 0, "Needle", needleIcon, "Can be used to make traps",false);
+	public ItemClass balloon_r = new ItemClass(8, 0, "Balloon", balloonIcon, "Can be used to make traps",false);
+	public ItemClass cbattery_r = new ItemClass(9, 0, "Car Battery", cbatteryIcon, "Can be used to make traps",false);
+	public ItemClass wheel_r = new ItemClass(10, 0, "Wheel", wheelIcon, "Can be used to make traps",false);
+	public ItemClass hanger_r = new ItemClass(11, 0, "Clothes Hanger", hangerIcon, "Can be used to make traps",false);
+	public ItemClass alcohol_r = new ItemClass(12, 0, "Alcohol", alcoholIcon, "Can be used to make traps",false);
 
 	// Traps
-	public ItemClass boxTrap_c = new ItemClass(21, 1, "Box Trap", boxTrapIcon, "Traps prey under box");
-	public ItemClass bow_c = new ItemClass(22, 2, "Bow", bowIcon, "Bow. Best used with arrows.");
+	public ItemClass boxTrap_c = new ItemClass(21, 1, "Box Trap", boxTrapIcon, "Traps prey under box", true);
+	public ItemClass bow_c = new ItemClass(22, 2, "Bow", bowIcon, "Bow. Best used with arrows.", true);
 
 	public class ItemClass
 	{
@@ -50,14 +50,16 @@ public class Item_Class : MonoBehaviour {
 		public string name;
 		public Texture2D icon;
 		public string description;
+		public bool isTrap;
 		
-		public ItemClass(int ItemIDe, int baitIDe, string name_e, Texture2D ico, string des)
+		public ItemClass(int ItemIDe, int baitIDe, string name_e, Texture2D ico, string des, bool isTrapE)
 		{
 			ItemID = ItemIDe;
 			baitID = baitIDe;
 			name = name_e;
 			icon = ico;
 			description = des;
+			isTrap = isTrapE;
 			
 		}
 
@@ -68,6 +70,7 @@ public class Item_Class : MonoBehaviour {
 			name = null;
 			icon = null;
 			description = null;
+			isTrap = false;
 
 		}
 		
---
+++ Assets/Scripts/JP_TrapSelection.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JP_TrapSelection : MonoBehaviour 
+{
+	public GameObject Box_Trap, Bow_Trap, Trap_C;
+
+	//Item_Class itemObject = new Item_Class();
+	public Item_Class.ItemClass itemObject = new Item_Class.ItemClass();
+
+	void Start()
+	{
+
+
+	}
+
+	public GameObject GetTrap(string name)
+	{
+		if(name == "Box Trap")
+		{
+			return Box_Trap;
+		}
+
+		if(name == "Bow")
+		{
+			return Bow_Trap;
+		}
+
+
+		return null;
+	}
+
+	public void SetTrap(Item_Class.ItemClass trapObject)
+	{
+		itemObject = trapObject;
+	}
+
+	public Item_Class.ItemClass GetTrap()
+	{
+		return itemObject;
+	}
+
+}
