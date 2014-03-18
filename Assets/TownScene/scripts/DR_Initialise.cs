0576da142c985b3ddd06c1926ef57c17be1197d3
DanRamsey21 <okislayer@gmail.com>
2014-03-18 23:49:42 +0000

    dan's wood scene

--- Assets/TownScene/scripts/DR_Initialise.cs
+++ Assets/TownScene/scripts/DR_Initialise.cs
@@ -12,24 +12,33 @@ public class DR_Initialise : MonoBehaviour {
 		PlayerPrefs.SetInt("QuestPart", 8);
 
 		//Tracking
-		PlayerPrefs.SetInt("Animal1", 1);
-		PlayerPrefs.SetInt("Animal2", 0);
-		PlayerPrefs.SetInt("Animal3", 1);
-		PlayerPrefs.SetInt("Animal4", 0);
-		PlayerPrefs.SetInt("Animal5", 0);
-		PlayerPrefs.SetInt("Animal6", 1);
-		PlayerPrefs.SetInt("Animal7", 0);
-		PlayerPrefs.SetInt("Animal8", 0);
-		PlayerPrefs.SetInt("Animal9", 1);
-		PlayerPrefs.SetInt("Animal10", 0);
-		PlayerPrefs.SetInt("Animal11", 0);
-		PlayerPrefs.SetInt("Animal12", 1);
-		PlayerPrefs.SetInt("Animal13", 1);
-		PlayerPrefs.SetInt("Animal14", 1);
-		PlayerPrefs.SetInt("Animal15", 0);
-		PlayerPrefs.SetInt("Animal16", 0);
-		PlayerPrefs.SetInt("Animal17", 1);
-		PlayerPrefs.SetInt("Animal18", 1);
+		PlayerPrefs.SetInt("Animal1", 0); // Mouse
+		PlayerPrefs.SetInt("Animal2", 0); // Spider
+		PlayerPrefs.SetInt("Animal3", 0); // Squirrel
+		PlayerPrefs.SetInt("Animal4", 0); // Skunk
+		PlayerPrefs.SetInt("Animal5", 0); // Hedgehog
+		PlayerPrefs.SetInt("Animal6", 0); // Owl
+		PlayerPrefs.SetInt("Animal7", 0); // Fish
+		PlayerPrefs.SetInt("Animal8", 0); // Beaver
+		PlayerPrefs.SetInt("Animal9", 0); // Bear
+		PlayerPrefs.SetInt("Animal10", 0); // Rabbit
+		PlayerPrefs.SetInt("Animal11", 0); // Deer
+		PlayerPrefs.SetInt("Animal12", 0); // Chicken
+		PlayerPrefs.SetInt("Animal13", 0); // Snake
+		PlayerPrefs.SetInt("Animal14", 0); // Eagle
+		PlayerPrefs.SetInt("Animal15", 0); // Lizard
+		PlayerPrefs.SetInt("Animal16", 0); // Dog
+		PlayerPrefs.SetInt("Animal17", 0); // Cat 
+		PlayerPrefs.SetInt("Animal18", 0); // Pidgeon
+		PlayerPrefs.SetInt("Animal19", 1);
+		PlayerPrefs.SetInt("Animal20", 1);
+		PlayerPrefs.SetInt("Animal21", 1);
+		PlayerPrefs.SetInt("Animal22", 1);
+		PlayerPrefs.SetInt("Animal23", 1);
+		PlayerPrefs.SetInt("Animal24", 1);
+		PlayerPrefs.SetInt("Animal25", 1);
+		PlayerPrefs.SetInt("Animal26", 1);
+		PlayerPrefs.SetInt("Animal27", 1);
 	}
 	
 	// Update is called once per frame
